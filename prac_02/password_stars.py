1698b832f5d395fd91e763b4483ef0367b447aa5
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-09-28 17:38:31 +0800

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,11 @@
+def main():
+    password = input("Enter password: ")
+
+    repeat_password = input("Repeat password: ")
+    while len(repeat_password) < len(password):
+        print("Length of password does not match.")
+        repeat_password = input("Repeat password: ")
+
+    print("*" * len(password))
+
+main()
