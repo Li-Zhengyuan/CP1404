831dabaab4f30eece30b2d34080a2b00fddc940a
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-09-28 18:10:23 +0800

    Add function to determine category

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,25 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+
+    category = determine_category(score)
+
+    print(f"Score status: {category}")
+
+
+def determine_category(score):
+    if score < 0 or score > 100:
+        category = "Invalid score"
+    elif score >= 90:
+        category = "Excellent"
+    elif score >= 50:
+        category = "Passable"
+    else:
+        category = "Bad"
+    return category
+
+
+main()
