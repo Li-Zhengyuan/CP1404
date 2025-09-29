c915ec37752e916e6b2fede88cbcef72fc248d51
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-09-29 10:57:43 +0800

    Rewrite the functions of the programs

--- prac_02/score.py
+++ prac_02/score.py
@@ -13,21 +13,28 @@ import random
 
 def main():
     """Print the category of the score and random score."""
-    score = float(input("Enter score: "))
+    score = get_score()
 
     category = determine_category(score)
 
-    print(f"Score status: {category}")
+    print(f"Score {score} category: {category}")
 
     random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     print(f"Random score {random_score} is {determine_category(random_score)}")
 
 
+def get_score():
+    """Get user input score."""
+    score = float(input("Enter score: "))
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
 def determine_category(score):
     """Determine the category of the score."""
-    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-        category = "Invalid score"
-    elif score >= EXCELLENT_THRESHOLD:
+    if score >= EXCELLENT_THRESHOLD:
         category = "Excellent"
     elif score >= PASSABLE_THRESHOLD:
         category = "Passable"
