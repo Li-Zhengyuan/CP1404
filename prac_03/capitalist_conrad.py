f95a8459453e8c31a13bb60dca4160e6941d102f
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-05 16:41:35 +0800

    Update the constants

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,13 +9,13 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0  # Lowest acceptable price
+MAX_PRICE = 100.0  # Highest acceptable price
 INITIAL_PRICE = 10.0
 
-FILENAME = ""
+FILENAME = "conrad.txt"
 
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
