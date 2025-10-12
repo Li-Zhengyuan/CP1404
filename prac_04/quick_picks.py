1d2e3c5d5baa9558cfc9726beb1f3223c01d6f08
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-12 19:47:01 +0800

    Generate required amount of numbers and display in ascending order

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,3 +9,15 @@ number_of_quick_picks = int(input("How many quick picks? "))
 while number_of_quick_picks < 0:
     print("It cannot less than 0")
     number_of_quick_picks = int(input("How many quick picks? "))
+
+for i in range(number_of_quick_picks):
+    quick_pick = []
+
+    for j in range(NUMBERS_PER_LINE):
+        random_number = random.randint(MINIMUM, MAXIMUM)
+        while random_number in quick_pick:
+            random_number = random.randint(MINIMUM, MAXIMUM)
+        quick_pick.append(random_number)
+    quick_pick.sort()
+
+    print(" ".join(f"{number:2}" for number in quick_pick))
