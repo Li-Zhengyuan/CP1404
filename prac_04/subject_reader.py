a8931c9035e505153c1aa47a2ee96ede593714cc
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-12 18:47:59 +0800

    Add a new function to display all subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,26 +7,30 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    """"""
+    subject_data = load_subject_data(FILENAME)
+    print(subject_data)
+    display_subject_details(subject_data)
 
 
-def load_data(filename=FILENAME):
+def load_subject_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     subject_records = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
         subject_records.append(parts)
     input_file.close()
     return subject_records
 
+
+def display_subject_details(subject_records):
+    """"""
+    name_width = max(len(record[1]) for record in subject_records)
+    count_width = max(len(str(record[2])) for record in subject_records)
+    for subject_code, lecturer_name, student_count in subject_records:
+        print(f"{subject_code} is taught by {lecturer_name:<{name_width}} and has {student_count:>{count_width}} students")
 
 main()
