ee91e12dd054b78f7d5f4f163697321a5a82309a
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-12 19:38:23 +0800

    Get number of quick picks with error checking

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,3 +3,9 @@ import random
 NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
+
+
+number_of_quick_picks = int(input("How many quick picks? "))
+while number_of_quick_picks < 0:
+    print("It cannot less than 0")
+    number_of_quick_picks = int(input("How many quick picks? "))
