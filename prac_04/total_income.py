fac4e684be46f35f5de3119f4a9902116f535a7c
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-12 20:02:18 +0800

    Use enumerate to pass one less parameters

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,22 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    month = int(input("How many months? "))
-    print()
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, month + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report(incomes)
+
+def print_report(incomes):
+    """Print report based on input incomes."""
     print("\nIncome Report\n-------------")
-    display_income_report(incomes, month)
-
-
-def display_income_report(incomes, month):
-    """Display income report for given incomes and number of months."""
     total = 0
-    for month in range(1, month + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, 1):
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f}         Total: ${total:10.2f}")
 
