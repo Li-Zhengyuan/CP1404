1e1a9f75d7201be465d241b849c3601c0ded2e1d
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 18:50:51 +0800

    Set the constant

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+NAME_TO_CODE = {
+    "Absolute Zero": "#0048ba",
+    "Acid Green": "#b0bf1a",
+    "AliceBlue": "#f0f8ff",
+    "Alizarin crimson": "#e32636",
+    "Amaranth": "#e52b50",
+    "Amber": "#ffbf00",
+    "Amethyst": "#9966cc",
+    "AntiqueWhite": "#faebd7",
+    "AntiqueWhite2": "#ffefdb",
+    "Deep Peach": "#ffcba4"
+}
