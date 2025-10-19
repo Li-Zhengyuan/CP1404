fc54a10d58d5b106782301cb4d4b9a68257c6d09
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 19:17:06 +0800

    Sort the output by keys

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,9 +1,8 @@
 """
 Word Occurrences
 Estimate: 30 minutes
-Actual:    minutes
+Actual:   20 minutes
 """
-from prac_05.state_names import max_length
 
 text = input("Text: ")
 
@@ -11,6 +10,9 @@ word_to_count = {}
 for word in text.split( ):
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-max_length = max(len(word) for word in word_to_count.keys())
-for word, count in word_to_count.items():
-    print(f"{word}: {count}")
+max_length = max(len(word) for word in word_to_count)
+
+sorted_words = sorted(word_to_count.keys())
+
+for word in sorted_words:
+    print(f"{word:{max_length}} : {word_to_count[word]}")
