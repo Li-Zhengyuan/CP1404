c2b753e9da63e5132b1fc051fda634ff68e2d888
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 19:04:06 +0800

    Print outputs in one nice column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,9 +3,14 @@ Word Occurrences
 Estimate: 30 minutes
 Actual:    minutes
 """
+from prac_05.state_names import max_length
 
 text = input("Text: ")
+
 word_to_count = {}
 for word in text.split( ):
     word_to_count[word] = word_to_count.get(word, 0) + 1
-print(word_to_count)
+
+max_length = max(len(word) for word in word_to_count.keys())
+for word, count in word_to_count.items():
+    print(f"{word}: {count}")
