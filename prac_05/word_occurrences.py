04e8de62205c4af6bd58b9542c7443602bea6d03
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 19:01:03 +0800

    Calculate the time each word appears

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,3 +4,8 @@ Estimate: 30 minutes
 Actual:    minutes
 """
 
+text = input("Text: ")
+word_to_count = {}
+for word in text.split( ):
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+print(word_to_count)
