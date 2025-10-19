0611368e2ee5a499d6cfa0ac731a8a672acacfe9
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 19:30:05 +0800

    Create a separate function to extract a name from the email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,24 @@ Emails
 Estimate: 30 minutes
 Actual:    minutes
 """
+
+
+def main():
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        email = input("Email: ")
+
+
+
+
+def extract_name(email):
+    name_part = email.split("@")
+    parts = name_part.split(".")
+    name = " ".join(parts).title()
+    return name
+
+
+
+main()
