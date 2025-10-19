ee8bade88bef469bf9aaea4f13a0c62ac97c3732
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 18:55:28 +0800

    Change to f string

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,20 +1,20 @@
 NAME_TO_CODE = {
-    "Absolute Zero": "#0048ba",
-    "Acid Green": "#b0bf1a",
-    "AliceBlue": "#f0f8ff",
-    "Alizarin crimson": "#e32636",
-    "Amaranth": "#e52b50",
-    "Amber": "#ffbf00",
-    "Amethyst": "#9966cc",
-    "AntiqueWhite": "#faebd7",
-    "AntiqueWhite2": "#ffefdb",
+    "ABSOLUTE ZERO": "#0048ba",
+    "ACID GREEN": "#b0bf1a",
+    "ALICEBLUE": "#f0f8ff",
+    "ALIZARIN CRIMSON: "#e32636",
+    "AMARANTH": "#e52b50",
+    "AMBER": "#ffbf00",
+    "AMETHYST": "#9966cc",
+    "ANTIQUEWHITE": "#faebd7",
+    "BLACK": "#000000",
     "Deep Peach": "#ffcba4"
 }
 
 name = input("Enter a name: ").upper()
 while name != "":
     try:
-        print(NAME_TO_CODE[name])
+        print(f"{name} is {NAME_TO_CODE[name]}")
     except KeyError:
-        print("Not a valid key.")
+        print("Not a valid name.")
     name = input("Enter a name: ").upper()
