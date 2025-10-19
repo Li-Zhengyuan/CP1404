20e8dee2ba3a66d7dabe61560c2bab29ecb9be82
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 20:06:54 +0800

    Work out the function to load data in a list

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,3 +5,19 @@ Actual:    minutes
 """
 
 FILENAME = "wimbledon.csv"
+
+def main():
+    pass
+
+
+
+def load_data(filename):
+    """Read CSV data return records as list."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # skip header
+        records = [line.strip().split(",") for line in in_file]
+    return records
+
+
+
+main()
