981c2fdc3e9e8b0b3d87a2e67938f7b049637ceb
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-10-19 19:39:27 +0800

    Enable user to enter the email to get the name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,15 +8,20 @@ Actual:    minutes
 def main():
     email_to_name = {}
 
-    email = input("Email: ")
+    email = input("Email: ").strip()
     while email != "":
-        email = input("Email: ")
+        name = extract_name(email)
+        answer = input(f"Is your name {name}? (Y/n)").upper()
+        if answer not in ("Y", ""):
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ").strip()
 
-
-
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 def extract_name(email):
-    name_part = email.split("@")
+    name_part = email.split("@")[0]
     parts = name_part.split(".")
     name = " ".join(parts).title()
     return name
