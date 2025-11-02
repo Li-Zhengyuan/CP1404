925d5942613a0854d4d5ae1126e4222857343e97
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-02 18:57:11 +0800

    Create get_age function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -11,3 +11,9 @@ class Guitar:
     def __str__(self):
         """Return a string representation of the Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        current_year = 2022
+        return current_year - self.year
