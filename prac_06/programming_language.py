c75d9147b86ab462f1806d6b650eb67dab7fc175
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-02 18:28:56 +0800

    Create init function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,21 @@
+"""
+Programming_language
+Estimate: 30 minutes
+Current Time: 18:16
+Actual:      minutes
+"""
+
+class ProgrammingLanguage:
+    """Programming language class."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a Programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+def is_dynamic(self):
+    """"""
+
