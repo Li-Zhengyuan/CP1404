d1ae6d707144d464fbd2455263f852000e43cd52
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-02 18:55:18 +0800

    Create __init__ function

---
+++ prac_06/guitar.py
@@ -0,0 +1,10 @@
+class Guitar:
+    """Guitar class."""
+
+    def __init__(self, name = "", year = 0, cost = 0):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
