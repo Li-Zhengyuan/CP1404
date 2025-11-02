8526381ab64f9a4ecd46b2eeefb1118d833ca676
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-02 18:25:09 +0800

    Input the starting code and import class

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+Languages
+Estimate: 30 minutes
+Current Time: 18:16
+Actual:      minutes
+"""
+
+from prac_06 import programming_language
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
