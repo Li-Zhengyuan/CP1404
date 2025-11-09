9904ead1149ab10826a6f9487d5c966ecb3dbfff
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-09 18:20:28 +0800

    Complete first part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -13,3 +13,21 @@ def main():
             cost = float(parts[2])
             guitar = Guitar(name, year, cost)
             guitars.append(guitar)
+
+    print("These are my guitars:")
+    for guitar in guitars:
+        print(f"{guitar}")
+    print()
+
+    guitars.sort()
+
+    print("Sorted by year (oldest to newest):")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>25} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
