7794e221bd33f8750dc8f65b8b29ab08493aa066
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-09 18:26:50 +0800

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,18 +1,13 @@
 from guitar import Guitar
 
+
+FILENAME = "guitars.csv"
+
+
 def main():
     """Read guitar data, store as Guitar objects, sort, and display."""
-    guitars = []
+    guitars = load_file(FILENAME)
 
-    # Read from CSV
-    with open('guitars.csv', 'r') as in_file:
-        for line in in_file:
-            parts = line.strip().split(',')
-            name = parts[0].strip()
-            year = int(parts[1])
-            cost = float(parts[2])
-            guitar = Guitar(name, year, cost)
-            guitars.append(guitar)
 
     print("These are my guitars:")
     for guitar in guitars:
@@ -27,7 +22,35 @@ def main():
             vintage_string = " (vintage)"
         else:
             vintage_string = ""
-        print(f"Guitar {i}: {guitar.name:>25} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        print(f"Guitar {i}: {guitar.name:>24} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+    print("Add new guitars:")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ")
+
+    # Save all guitars back to the CSV
+    with open('guitars.csv', 'w') as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
+
+def load_file(filename):
+    guitars = []
+    with open(filename, 'r') as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            name = parts[0].strip()
+            year = int(parts[1])
+            cost = float(parts[2])
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+    return guitars
 
 
 main()
