c44e7cb444081ef5d92a58c18e8e96216a57cc0f
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-16 20:19:52 +0800

    Finish the left part of the program

--- prac_08/convert_m_to_km_solution.py
+++ prac_08/convert_m_to_km_solution.py
@@ -27,11 +27,15 @@ class MilesConverterApp(App):
         self.root = Builder.load_file('convert_m_km_solution.kv')
         return self.root
 
+
+
     def handle_calculate(self):
         """ handle calculation (could be button press or other call), output result to label widget """
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
+
+
 
     def handle_increment(self, change):
         """
@@ -41,6 +45,8 @@ class MilesConverterApp(App):
         value = self.get_validated_miles() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_calculate()
+
+
 
     def get_validated_miles(self):
         """
