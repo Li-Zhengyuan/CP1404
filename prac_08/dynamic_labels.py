e873feda7cd87de538ce9c2a0d1ae26ade37a07e
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-16 20:35:17 +0800

    Create a list of names

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,3 +1,14 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.lang import Builder
+
+
+class DynamicLabelsApp(App):
+    """Main app to dynamically create labels."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Li", "Zheng", "Yuan"]
+
+
+DynamicLabelsApp().run()
