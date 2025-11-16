cd3687fba1b20968e93f79cc8efd1feb6718ac6c
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-16 20:38:38 +0800

    Finish the build function

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -11,6 +11,15 @@ class DynamicLabelsApp(App):
         super().__init__(**kwargs)
         self.names = ["Li", "Zheng", "Yuan"]
 
+    def build(self):
+        """Build the Kivy GUI."""
+        self.root = Builder.load_file("dynamic_labels.kv")
+        main_layout = self.root.ids.main
 
+        for name in self.names:
+            temp_label = Label(text=name)
+            main_layout.add_widget(temp_label)
+
+        return self.root
 
 DynamicLabelsApp().run()
