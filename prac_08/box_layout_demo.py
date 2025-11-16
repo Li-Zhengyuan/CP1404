d5e6a8828ea0ebf732361262bd366e79c023a1fd
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-16 19:13:52 +0800

    Add a new event handler to reset the text field and the output label

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 
 
+
 class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
@@ -12,6 +13,12 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
 
 
 
