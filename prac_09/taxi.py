e8c5cb06feefc24acfa64a65cba8a7a205584325
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-23 16:57:36 +0800

    Adjust the layout

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -15,9 +15,13 @@ class Taxi(Car):
         super().__init__(name, fuel)
         self.current_fare_distance = 0
 
+
+
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+
+
 
     def get_fare(self):
         """Return the price for the taxi trip."""
@@ -25,9 +29,12 @@ class Taxi(Car):
         return fare
 
 
+
     def start_fare(self):
         """Begin a new fare."""
         self.current_fare_distance = 0
+
+
 
     def drive(self, distance):
         """Drive like parent Car but calculate fare distance as well."""
