14f649503624dee3b4e76f8e8fb67f9bde3e7909
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-23 17:05:13 +0800

    Restart fare and drive another 100 km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,3 +5,12 @@ my_taxi = Taxi("Prius 1", 100)
 
 # 2. Drive 40 km
 my_taxi.drive(40)
+
+# 3. Print taxi details and current fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare()}")
+
+
+# 4. Restart fare and drive another 100 km
+my_taxi.start_fare()
+my_taxi.drive(100)
