504ed28faf69a982bcad892456e0e7a2e8b5bb4c
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-23 19:44:36 +0800

    Display taxis and check if it is available

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,17 +5,21 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 
 MENU = "q)uit, c)hoose taxi, d)rive"
-
+TAXIS = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
 
 
 def main():
     """Run the Taxi Simulator program."""
     total_bill = 0
-    taxis = [
-        Taxi("Prius", 100),
-        SilverServiceTaxi("Limo", 100, 2),
-        SilverServiceTaxi("Hummer", 200, 4)
-    ]
+    # taxis = [
+    #     Taxi("Prius", 100),
+    #     SilverServiceTaxi("Limo", 100, 2),
+    #     SilverServiceTaxi("Hummer", 200, 4)
+    # ]
     current_taxi = None
 
     print("Let's drive!")
@@ -24,7 +28,9 @@ def main():
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            print("Taxis available: ")
+            display_taxis(TAXIS)
+            current_taxi = check_taxi(TAXIS)
         elif choice == "d":
             pass
         else:
@@ -32,6 +38,28 @@ def main():
 
         print(MENU)
         menu_choice = input(">>> ").lower()
+
+
+
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(TAXIS):
+        print(f"{i} - {taxi}")
+
+
+
+def check_taxi(taxis):
+    """Check if taxi is available."""
+    try:
+        taxi_choice = int(input("Choose taxi: "))
+        if 0 <= taxi_choice < len(taxis):
+            return taxis[taxi_choice]
+        else:
+            print("Invalid taxi choice")
+            return None
+    except ValueError:
+        print("Invalid taxi choice")
+        return None
 
 
 
