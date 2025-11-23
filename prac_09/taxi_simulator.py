9556e60917224ed103a04eb8c250a81af7a6b811
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-23 19:04:15 +0800

    Build the framework of the menu

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,4 +2,37 @@ from prac_09.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
+
+
 MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+
+def main():
+    """Run the Taxi Simulator program."""
+    total_bill = 0
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    current_taxi = None
+
+    print("Let's drive!")
+    print(MENU)
+
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid option")
+
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+
+
+main()
