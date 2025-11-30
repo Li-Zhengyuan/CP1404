fd6f5f86c184c4d1481be633edff3703459a8498
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-30 17:22:30 +0800

    Finish the try part

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,9 +4,14 @@ def main():
     title = input("Enter page title: ").strip()
     while title != "":
         try:
+            page = wikipedia.page(title, auto_suggest=False)
 
+            print(page.title)
+            print(page.summary.split("\n")[0])
+            print(page.url)
 
         except
+
         except
 
         title = input("\nEnter page title: ").strip()
