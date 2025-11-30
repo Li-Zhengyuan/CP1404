2cd795ca4dfb4939f67b4d847de5dbc1e3b4b4f3
Li-Zhengyuan <zhengyuan.li@my.jcu.edu.au>
2025-11-30 17:22:13 +0800

    Build the try except framework

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,1 +1,17 @@
 import wikipedia
+
+def main():
+    title = input("Enter page title: ").strip()
+    while title != "":
+        try:
+
+
+        except
+        except
+
+        title = input("\nEnter page title: ").strip()
+
+    print("Thank you.")
+
+
+main()
